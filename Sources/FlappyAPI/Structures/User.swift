95197d320fe986fb6dcfce29f5442c033fc4fce7
Brandon Plank <52553007+brandonplank@users.noreply.github.com>
2021-10-16 13:02:20 -0400

    owner

--- Sources/FlappyAPI/Structures/User.swift
+++ Sources/FlappyAPI/Structures/User.swift
@@ -22,8 +22,9 @@ public extension FlappyAPI {
         public var isBanned: Bool?
         public var banReason: String?
         public var admin: Bool?
+        public var owner: Bool?
         
-        public init(id: UUID? = UUID(), name: String, score: Int? = 0, deaths: Int? = 0, passwordHash: String, jailbroken: Bool? = false, hasHackedTools: Bool? = false, ranInEmulator: Bool? = false, hasModifiedScore: Bool? = false, isBanned: Bool? = false, banReason: String? = nil, admin: Bool? = false) {
+        public init(id: UUID? = UUID(), name: String, score: Int? = 0, deaths: Int? = 0, passwordHash: String, jailbroken: Bool? = false, hasHackedTools: Bool? = false, ranInEmulator: Bool? = false, hasModifiedScore: Bool? = false, isBanned: Bool? = false, banReason: String? = nil, admin: Bool? = false, owner: Bool? = false) {
             self.id = id
             self.name = name
             self.score = score
@@ -36,6 +37,7 @@ public extension FlappyAPI {
             self.isBanned = isBanned
             self.banReason = banReason
             self.admin = admin
+            self.owner = owner
         }
     }
 
